91bd50ca016843725b72fc0f9e26dc881b2c803b
unknown <your@email.example>
2024-10-22 13:00:06 +0300

    show route fixed; switched with /create

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -8,6 +8,6 @@ Route::get('/', function () {
 });
 
 Route::get('/posts', [PostController::class, 'index']);
-Route::get('/posts/{post}/show', [PostController::class, 'show']);
 Route::get('/posts/create', [PostController::class, 'create']);
+Route::get('/posts/{post}', [PostController::class, 'show']);
 Route::post('/posts', [PostController::class, 'store']);
